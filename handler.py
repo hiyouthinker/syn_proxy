10ed5987acbb8dad9f239b79bb28f75d5e2e1694
hiyouthinker <hi.youthinker@gmail.com>
2021-05-13 14:14:42 +0000

    change type of value of dictionary from list to dictionary

--- handler.py
+++ handler.py
@@ -64,7 +64,13 @@ def tcp_packet_handler(pkt, dir):
 					# ack is initial seq of Proxy -> Client
 					ack = pkt[TCP].ack - 1
 					# OK, this is a valid client, create the session
-					value = [tcp_state.TCP_SYN_SENT, ack, time.time(), 0, 0, pkt[TCP].window, 0]
+					value = {"state" : tcp_state.TCP_SYN_SENT,
+								"isn" : ack,
+								"time" : time.time(),
+								"flags" : 0,
+								"substate" : 0,
+								"window" : pkt[TCP].window,
+								"offset" : 0}
 					tcp_state.sessions[key] = value
 					utils.send_syn_to_server(sip, dip, sport, dport, seq, pkt[TCP].window)
 			elif (len > 0):
@@ -76,17 +82,17 @@ def tcp_packet_handler(pkt, dir):
 				print "invalid packet, drop the packet"
 	else :
 		value = tcp_state.sessions.get(key)
-		state = value[0]
-		offset = value[6]
-		now = value[2]
-		session_flags = value[3]
+		state = value["state"]
+		offset = value["offset"]
+		now = value["time"]
+		session_flags = value["flags"]
 		print "current state of session: %s" % (tcp_state.tcp_session_states[state])
 		# SYN
 		if (type[0] == tcp_state.TCP_TYPE_SYN):
 			if (time.time() - now > utils.tcp_session_timeout[state][0]) :
 				print "session timeout"
 				utils.send_synack_to_client(pkt)
-				value[3] = tcp_state.TCP_SESSION_FLAG_SEEN_SYN
+				value["flags"] = tcp_state.TCP_SESSION_FLAG_SEEN_SYN
 				tcp_state.sessions[key] = value
 			else :
 				print "session isn't expired, drop the SYN"
@@ -100,21 +106,27 @@ def tcp_packet_handler(pkt, dir):
 				if (state == tcp_state.TCP_ESTABLISHED):
 					print "received retransmitted SYN + ACK"
 				else :
-					# value[1] is initial seq number from SYN Proxy to Client
-					offset = seq - value[1]
+					# value["isn"] is initial seq number from SYN Proxy to Client
+					offset = seq - value["isn"]
 				# update the session
-				value = [tcp_state.TCP_ESTABLISHED, value[1], time.time(), 0, 0, value[5], offset]
+				value = {"state" : tcp_state.TCP_ESTABLISHED,
+							"isn" : value["isn"],
+							"time" : time.time(),
+							"flags" : 0,
+							"substate" : 0,
+							"window" : value["window"],
+							"offset" : offset}
 				tcp_state.sessions[key] = value
 				print "TCP 6-way handshake with client/server was completed successfully"
 				print "send ACK to backend"
-				utils.send_ack_to_server(dip, sip, dport, sport, ack, seq+1, value[5])
+				utils.send_ack_to_server(dip, sip, dport, sport, ack, seq+1, value["window"])
 		# ACK
 		elif (type[0] == tcp_state.TCP_TYPE_ACK):
 			if ((dir == 0) and ((session_flags & tcp_state.TCP_SESSION_FLAG_SEEN_SYN) or (state == tcp_state.TCP_SYN_SENT))):
 				if (utils.tcp_syn_cookie_check(pkt[TCP].ack) == False):
 					if (session_flags & tcp_state.TCP_SESSION_FLAG_SEEN_SYN) :
 						print "ACK verification failed, forward the packet to backend"
-						value[3] &= ~tcp_state.TCP_SESSION_FLAG_SEEN_SYN
+						value["flags"] &= ~tcp_state.TCP_SESSION_FLAG_SEEN_SYN
 						tcp_state.sessions[key] = value
 						utils.forwar_pkt_to_client_server(key, value, dir, pkt, offset)
 						return
@@ -125,8 +137,14 @@ def tcp_packet_handler(pkt, dir):
 					seq = pkt[TCP].seq - 1 + 1
 					ack = pkt[TCP].ack - 1
 					# update state and seq of SYN Proxy and time
-					# value[6] is offset, the value MUST NOT be changed before receiving the SYN+ ACK from the server
-					value = [tcp_state.TCP_SYN_SENT, ack, time.time(), 0, 0, pkt[TCP].window, value[6]]
+					# value["offset"] is offset, the value MUST NOT be changed before receiving the SYN+ ACK from the server
+					value = {"state" : tcp_state.TCP_SYN_SENT,
+								"isn" : ack,
+								"time" : time.time(),
+								"flags" : 0,
+								"substate" : 0,
+								"window" : pkt[TCP].window,
+								"offset" : value["offset"]}
 					tcp_state.sessions[key] = value
 					print "Valid ACK, I will reconect to backend"
 					utils.send_syn_to_server(sip, dip, sport, dport, seq, pkt[TCP].window)
--- utils.py
+++ utils.py
@@ -46,15 +46,15 @@ def forwar_pkt_to_client_server(key, value, dir, pkt, offset):
 	dport = pkt[TCP].dport
 	flags = pkt[TCP].flags
 	type = tcp_state.tcp_flags_check(flags)
-	state = value[0]
-	substate = value[4]
+	state = value["state"]
+	substate = value["substate"]
 	window = pkt[TCP].window
 	target = tcp_state.tcp_pkt_flags[type[0] + type[1]]
 
 	len = get_tcp_payload_length(pkt)
 
 	# fresh the time
-	value[2] = time.time()
+	value["time"] = time.time()
 	tcp_state.sessions[key] = value
 
 	# ACK/FIN
@@ -89,9 +89,9 @@ def forwar_pkt_to_client_server(key, value, dir, pkt, offset):
 				substate |= tcp_state.tcp_session_server_rst
 			substate |= tcp_state.TCP_SESSION_SUBSTATE_CLOSED
 
-	if (substate != value[4]):
-		value[0] = tcp_state.TCP_FIN_WAIT
-		value[4] = substate
+	if (substate != value["substate"]):
+		value["state"] = tcp_state.TCP_FIN_WAIT
+		value["substate"] = substate
 		tcp_state.sessions[key] = value
 
 	if (dir == 0):
@@ -120,8 +120,8 @@ def forwar_pkt_to_client_server(key, value, dir, pkt, offset):
 			if (state == tcp_state.TCP_SYN_SENT):
 				print "The port of server is not open? (seq: %d)" % seq
 				# This is a RST + ACK for syn
-				# seq (value[1]) is 0, need to correct the seq number
-				seq = value[1] + 1
+				# seq (value["isn"]) is 0, need to correct the seq number
+				seq = value["isn"] + 1
 			else :
 				# for example: When the connection has been closed, the server received the data from the client
 				seq -= offset
@@ -172,37 +172,37 @@ def show_tcp_all_sessions():
 	print "\nsession table: %d item(s)" % len(keys)
 	for key in keys :
 		value = tcp_state.sessions.get(key)
-		state = value[0]
+		state = value["state"]
 		status = "Active"
 
-		if (value[3] & tcp_state.TCP_SESSION_FLAG_EXPIRED):
+		if (value["flags"] & tcp_state.TCP_SESSION_FLAG_EXPIRED):
 			status = "Expired"
-		if (value[3] & tcp_state.TCP_SESSION_FLAG_SEEN_SYN):
+		if (value["flags"] & tcp_state.TCP_SESSION_FLAG_SEEN_SYN):
 			status += " & SEEN SYN"
 
 		if (state == tcp_state.TCP_FIN_WAIT):
 			substate = ""
-			if (value[4] & 0xc0):
-				substate = tcp_state.tcp_session_destroy_first_pkt_dir[value[4] & 0xc0]
-			if (value[4] & 0x30):
+			if (value["substate"] & 0xc0):
+				substate = tcp_state.tcp_session_destroy_first_pkt_dir[value["substate"] & 0xc0]
+			if (value["substate"] & 0x30):
 				if (len(substate) != 0):
-					substate += "/" + tcp_state.tcp_session_destroy_first_pkt_dir[value[4] & 0x30]
+					substate += "/" + tcp_state.tcp_session_destroy_first_pkt_dir[value["substate"] & 0x30]
 				else :
-					substate = tcp_state.tcp_session_destroy_first_pkt_dir[value[4] & 0x30]
+					substate = tcp_state.tcp_session_destroy_first_pkt_dir[value["substate"] & 0x30]
 			print ("\t[%s:%d => %s:%d], last_time: %d, offset: %d, status: %s, state: %s/0x%02x (%s)"
-				% (key[0], key[1], key[2], key[3], value[2], value[6], status,
+				% (key[0], key[1], key[2], key[3], value["time"], value["offset"], status,
 				tcp_state.tcp_session_states[state],
-				(value[4] & 0x0f), substate))
+				(value["substate"] & 0x0f), substate))
 		else :
 			print ("\t[%s:%d => %s:%d], last_time: %d, offset: %s, status: %s, state: %s"
-				% (key[0], key[1], key[2], key[3], value[2], value[6], status, tcp_state.tcp_session_states[state]))
+				% (key[0], key[1], key[2], key[3], value["time"], value["offset"], status, tcp_state.tcp_session_states[state]))
 
-		if ((time.time() - value[2]) > tcp_session_timeout[state][1]):
+		if ((time.time() - value["time"]) > tcp_session_timeout[state][1]):
 			print "\t\t(this session was expired, will be removed)"
 			del tcp_state.sessions[key]
-		elif ((time.time() - value[2]) > tcp_session_timeout[state][0]):
-			value[3] |= tcp_state.TCP_SESSION_FLAG_EXPIRED
+		elif ((time.time() - value["time"]) > tcp_session_timeout[state][0]):
+			value["flags"] |= tcp_state.TCP_SESSION_FLAG_EXPIRED
 			tcp_state.sessions[key] = value
-		elif (value[3] & tcp_state.TCP_SESSION_FLAG_EXPIRED):
-			value[3] &= ~tcp_state.TCP_SESSION_FLAG_EXPIRED
+		elif (value["flags"] & tcp_state.TCP_SESSION_FLAG_EXPIRED):
+			value["flags"] &= ~tcp_state.TCP_SESSION_FLAG_EXPIRED
 			tcp_state.sessions[key] = value
